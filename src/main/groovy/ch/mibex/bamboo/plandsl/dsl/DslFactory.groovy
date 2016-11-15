e09de88e80eab3692908f3b1e8454729279025d8
Michael Rüegg <rueegg.michael@gmail.com>
2016-11-15 11:56:12 +0100

    Support for standalone job definitions

--- src/main/groovy/ch/mibex/bamboo/plandsl/dsl/DslFactory.groovy
+++ src/main/groovy/ch/mibex/bamboo/plandsl/dsl/DslFactory.groovy
@@ -1,5 +1,7 @@
 package ch.mibex.bamboo.plandsl.dsl
 
+import ch.mibex.bamboo.plandsl.dsl.jobs.Job
+
 interface DslFactory {
 
     /**
@@ -9,4 +11,13 @@ interface DslFactory {
      */
     Project project(String key, @DelegatesTo(Project) Closure closure)
 
+    /**
+     * Creates a build job definition.
+     *
+     * @param key the key of the job consisting of an uppercase letter followed by one or more uppercase
+     * alphanumeric characters. E. g. CORE (for a module called core)
+     */
+    Job job(String key, @DelegatesTo(Job) Closure closure)
+
+    Job job(String key)
 }
--- src/main/groovy/ch/mibex/bamboo/plandsl/dsl/DslScript.groovy
+++ src/main/groovy/ch/mibex/bamboo/plandsl/dsl/DslScript.groovy
@@ -1,5 +1,7 @@
 package ch.mibex.bamboo.plandsl.dsl
 
+import ch.mibex.bamboo.plandsl.dsl.jobs.Job
+
 abstract class DslScript extends Script implements DslFactory {
     final Set<Project> projects = new LinkedHashSet<>()
     BambooFacade bambooFacade
@@ -14,4 +16,19 @@ abstract class DslScript extends Script implements DslFactory {
         project
     }
 
+    @Override
+    Job job(String key, @DelegatesTo(Job) Closure closure) {
+        def job = new Job(bambooFacade)
+        job.key(key)
+        DslScriptHelper.execute(closure, job)
+        job.validate()
+        job
+    }
+
+    @Override
+    Job job(String key) {
+        def job = new Job(bambooFacade)
+        job.key(key)
+        job
+    }
 }
